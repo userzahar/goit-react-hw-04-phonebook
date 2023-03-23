a6dffdeda3591757630e1143554e78db7d77d3e0
userzahar <110730909+userzahar@users.noreply.github.com>
2023-03-23 23:23:08 +0200

    refractoring form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,62 +1,63 @@
 import { Button } from "components/Button/Button";
 import { LabelStyled } from "components/Filter/FilterStyled";
 import { nanoid } from "nanoid";
+import { useState } from "react";
 import { InputStyled } from "./StyledInput";
-const { Component } = require("react");
 
-export class Form extends Component {
-    state = {
-        name: '',
-        number: ''
-    }
-    inputId = nanoid();
-    inputIdNew = nanoid();
-    handleChange = ({ target }) => {
-        this.setState({ [target.name]: target.value })
-    }
-    handleSubmit = (e) => {
-        e.preventDefault();
-        let cteateContact = this.props.createContacts({
-            name: this.state.name,
-            number: this.state.number
-        })
-        if (cteateContact === undefined) {
-            this.reset();
+export function Form({ createContacts }) {
+    const [name, setName] = useState('');
+    const [number, setNumber] = useState('')
+
+    const inputId = nanoid();
+    const inputIdNew = nanoid();
+    const handleChange = ({ target }) => {
+        switch (target.name) {
+            case "name": setName(target.value);
+                break;
+            case "number": setNumber(target.value);
+            default: ;
         }
     }
-    reset = () => {
-        this.setState({
-            name: '',
-            number: ''
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let cteateContact = createContacts({
+            name: name,
+            number: number
         })
+        if (cteateContact === undefined) {
+           reset();
+        }
     }
-    render() {
-        return <form onSubmit={this.handleSubmit}>
-            <LabelStyled htmlFor={this.inputId}>Name</LabelStyled>
+     function reset()  {
+         setName('');
+         setNumber('');
+    }
+        return <form onSubmit={handleSubmit}>
+            <LabelStyled htmlFor={inputId}>Name</LabelStyled>
             <InputStyled
                 type="text"
                 name="name"
                 pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                 title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
                 required
-                onChange={this.handleChange}
-                value={this.state.name}
-                id={this.inputId}
+                onChange={handleChange}
+                value={name}
+                id={inputId}
             />
 
-            <LabelStyled htmlFor={this.inputIdNew}>Number</LabelStyled>
+            <LabelStyled htmlFor={inputIdNew}>Number</LabelStyled>
             <InputStyled
                 type="tel"
                 name="number"
                 pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
                 title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
                 required
-                onChange={this.handleChange}
-                value={this.state.number}
-                id={this.inputIdNew}
+                onChange={handleChange}
+                value={number}
+                id={inputIdNew}
             />
             <Button text="Add Contact" />
         </form>
 
-    }
+    
 }
