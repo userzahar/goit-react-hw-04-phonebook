d72ac799128473e2d3471189f88507c90ef30475
userzahar <110730909+userzahar@users.noreply.github.com>
2023-03-24 18:13:47 +0200

    ad callback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,7 @@ import { useState,useEffect } from "react";
 
 export function App () {
   const [filter, setFilter] = useState('');
-  const[contacts, setContacts] = useState(JSON.parse(localStorage.getItem('contacts')) || [
+  const[contacts, setContacts] = useState(()=>JSON.parse(localStorage.getItem('contacts')) || [
     { id: 'id-1', name: 'Volodymyr Zelenskyi', number: '+380-459-12-5678' },
     { id: 'id-2', name: 'Petro Poroshenko', number: '+380-443-89-1256' },
     { id: 'id-3', name: 'Viktor Yushchenko', number: '+380-645-17-7943' }])
